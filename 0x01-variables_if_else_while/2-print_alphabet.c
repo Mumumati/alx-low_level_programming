a52c9396d306c67ef0452a78feb62df5ff73a1d7
Mumumati <munasheitayi@gmail.com>
2022-07-01 06:26:56 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - main block
- *
- *   * Description: Use `putchar` function to print the alphabet in lowercase.
- *
- *    * Return: 0
- *
- *     */
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
 
 int main(void)
-
 {
-
-		char alpha = 'a';
-
-
-
-			while (alpha <= 'z')
-
-					{
-
-								putchar(alpha);
-
-										alpha++;
-
-											}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
 }
