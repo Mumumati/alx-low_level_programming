7779e8e041277ab65b25917cfb49b5e6a394cd60
Mumumati <munasheitayi@gmail.com>
2022-07-06 01:43:14 -0700

    My first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
-int main()
+#include "main.h"
+/**
+ * main - prints a string of characters
+ * Description: prints _putchar, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+
 {
-	printf("_putchar\n");
-		return 0;
+
+	char a[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+
+	{
+
+		_putchar(a[i]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
 }
