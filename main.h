657c7cd6e3a9ab64457837aa9d3d9dafef36d466
Mumumati <munasheitayi@gmail.com>
2022-07-08 00:47:00 -0700

    More functions, more nested loops

--- main.h
+++ main.h
@@ -2,20 +2,28 @@
 
 #define MAIN_H
 
+int _putchar(char c);
 
+int _isupper(int c);
 
-#include <stdio.h>
+int _isdigit(int c);
 
+int mul(int a, int b);
 
+void print_numbers(void);
 
-void positive_or_negative(int i);
+void print_most_numbers(void);
 
-int largest_number(int a, int b, int c);
+void more_numbers(void);
 
-void print_remaining_days(int month, int day, int year);
+void print_line(int n);
 
-int convert_day(int month, int day);
+void print_diagonal(int n);
 
+void print_square(int size);
 
+void print_triangle(int size);
+
+void print_number(int n);
 
 #endif /* MAIN_H */
