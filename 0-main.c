9929079aa1541b18e2860fc003646971bdd6afbc
Mumumati <munasheitayi@gmail.com>
2022-07-08 00:53:43 -0700

    More functions, more nested loops

--- 0-main.c
+++ 0-main.c
@@ -1,15 +1,19 @@
 #include "main.h"
+
+#include <stdio.h>
 /**
+ *main - check the code.
  *
- *  * main - tests function that prints if integer is positive or negative
- *
- *   * Return: 0
- *
- *    */
+ *Return: Always 0.
+ */
+
 int main(void)
 {
-int i;
-i = 0;		
-positive_or_negative(i);
-return (0)
+	char c;
+	
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
